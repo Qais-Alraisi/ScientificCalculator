4b26552e8625a37e0622a5200b65413f187e6bc3
Qais Al Raisi <23-0622@student.gutech.edu.om>
2025-07-20 16:35:51 +0400

    Add & Subtract

--- ScientificCalculator/src/Main.java
+++ ScientificCalculator/src/Main.java
@@ -8,10 +8,24 @@ public class Main {
             System.out.print("Select an Arithmetic operation:\n__________________________________________________________\n| 1a = add | 2a = subtract | 3a = multiply | 4a = divide |\n\nSelect a  Scientific operation:\n________________________________________________________________________________________________________________________________________________________________________________________________________________\n| 1b = Power | 2b = Sine | 3b = Cosine | 4b = Tangent | 5b = Natural Logarithm | 6b = Logarithm | 7b = Absolute Value | 8b = Round (nearest long) | 9b = Ceiling | 10b = Floor | 11b = Minimum | 12b = Maximum | \nControl & Utilities:\n________________________________________________________\n| 1c  = Display Menu | 2c = Helper Methods | 3c = Exit |\n\n");
         }
     }
-    public static void addition(){
+
+    public static void addition() {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter first number: ");
-        double a =
+        double a = scanner.nextDouble();
+        System.out.print("Enter second number: ");
+        double b = scanner.nextDouble();
+        double result = a + b;
+        System.out.print("The Sum of " + a + " and " + b + " is " + result);
+    }
 
+    public static void subtraction() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        double a = scanner.nextDouble();
+        System.out.print("Enter second number: ");
+        double b = scanner.nextDouble();
+        double result = a - b;
+        System.out.print("The subtraction of " + a + " and " + b + " is " + result);
     }
 }
