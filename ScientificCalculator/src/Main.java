2c55c6a877ef85e1123fd99c195a14ab68c8a12e
Qais Al Raisi <23-0622@student.gutech.edu.om>
2025-07-20 16:22:31 +0400

    First commit

---
+++ ScientificCalculator/src/Main.java
@@ -0,0 +1,17 @@
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        new Scanner(System.in);
+        while (true) {
+            System.out.print("Select an Arithmetic operation:\n__________________________________________________________\n| 1a = add | 2a = subtract | 3a = multiply | 4a = divide |\n\nSelect a  Scientific operation:\n________________________________________________________________________________________________________________________________________________________________________________________________________________\n| 1b = Power | 2b = Sine | 3b = Cosine | 4b = Tangent | 5b = Natural Logarithm | 6b = Logarithm | 7b = Absolute Value | 8b = Round (nearest long) | 9b = Ceiling | 10b = Floor | 11b = Minimum | 12b = Maximum | \nControl & Utilities:\n________________________________________________________\n| 1c  = Display Menu | 2c = Helper Methods | 3c = Exit |\n\n");
+        }
+    }
+    public static void addition(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        double a =
+
+    }
+}
